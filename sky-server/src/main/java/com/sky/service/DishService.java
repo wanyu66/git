3c4546e763bbfe66633361a116d60f9dd2b79c4a
wanyu66 <1816755214@qq.com>
2026-06-17 16:48:36 +0800

    新增商品浏览代码实现

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -47,4 +47,13 @@ public interface DishService {
      * @return
      */
     List<Dish> list(Long categoryId);
+
+
+        /**
+         * 条件查询菜品和口味
+         * @param dish
+         * @return
+         */
+    List<DishVO> listWithFlavor(Dish dish);
+
 }
