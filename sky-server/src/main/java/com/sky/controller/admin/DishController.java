ccf9b77f44257fd2e8fa20025b4a68a1a19e6a1d
wanyu66 <1816755214@qq.com>
2026-06-20 14:36:10 +0800

    新增缓存菜品代码实现

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -25,6 +27,8 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 新增菜品
      * @param dishDTO
@@ -35,6 +39,8 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("添加菜品：{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        String key = "dish_"+dishDTO.getCategoryId();
+        cleanCachs(key);
         return Result.success();
     }
     /**
@@ -59,6 +65,7 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品批量删除：{}",ids);
         dishService.deleteBatch(ids);
+        cleanCachs("dish_*");
         return Result.success();
     }
     /**
@@ -78,6 +85,7 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("编辑菜品信息：{}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
+        cleanCachs("dish_*");
         return Result.success();
     }
 
@@ -92,5 +100,9 @@ public class DishController {
         log.info("根据分类id查询菜品：{}", categoryId);
         List<Dish> list = dishService.list(categoryId);
         return Result.success(list);
+    }
+    private void cleanCachs(String  pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 }
